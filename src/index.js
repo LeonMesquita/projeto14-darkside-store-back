f866826a145137556dc92edf654c95db8be35064
LeonMesquita <leonmesquita16@gmail.com>
2022-07-07 15:18:28 -0300

    cors

--- src/index.js
+++ src/index.js
@@ -39,7 +39,7 @@ app.use(express.json());
 
 app.use(productRouter);
 
-
-app.listen(process.env.PORT, () => {
-    console.log("Server running on port " + process.env.PORT);
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log("Server running on port " + PORT);
 });
