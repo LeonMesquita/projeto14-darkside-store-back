1fe232755d275ce53a10fb75763c5b7a881cfe1a
LeonMesquita <leonmesquita16@gmail.com>
2022-07-07 14:54:02 -0300

    cors

--- src/index.js
+++ src/index.js
@@ -1,17 +1,35 @@
 import express from 'express';
-import cors from 'cors';
 import dotenv from 'dotenv';
 import productRouter from './routes/productRouter.js';
 
 dotenv.config();
 const app = express();
-app.use(cors());
+
+
+//Cors Configuration - Start
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested, Content-Type, Accept Authorization"
+    )
+    if (req.method === "OPTIONS") {
+      res.header(
+        "Access-Control-Allow-Methods",
+        "POST, PUT, PATCH, GET, DELETE"
+      )
+      return res.status(200).json({})
+    }
+    next()
+  })
+/*
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
     next();
 });
+*/
 app.use(express.json());
 
 
