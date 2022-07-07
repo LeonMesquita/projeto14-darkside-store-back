c1efd420947a8a0586c7be535922cf83035fd0aa
LeonMesquita <leonmesquita16@gmail.com>
2022-07-07 14:42:22 -0300

    make it better

--- src/index.js
+++ src/index.js
@@ -5,8 +5,15 @@ import productRouter from './routes/productRouter.js';
 
 dotenv.config();
 const app = express();
-app.use(express.json());
 app.use(cors());
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
+app.use(express.json());
+
 
 
 app.use(productRouter);
