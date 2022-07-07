7b97d2a36fb4a2de3977065669d56727df613f49
LeonMesquita <leonmesquita16@gmail.com>
2022-07-07 14:58:59 -0300

    cors

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import productRouter from './routes/productRouter.js';
-
+import cors from 'cors';
 dotenv.config();
 const app = express();
 
@@ -30,6 +30,7 @@ app.use((req, res, next) => {
     next();
 });
 */
+app.use(cors());
 app.use(express.json());
 
 
