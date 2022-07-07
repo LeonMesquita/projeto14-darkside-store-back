f20a60bc79b1c0bd4fcfe57b67e3fcca4b4d249b
LeonMesquita <leonmesquita16@gmail.com>
2022-07-07 15:12:24 -0300

    cors

--- src/index.js
+++ src/index.js
@@ -23,15 +23,16 @@ const app = express();
 //     next()
 //   })
 
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
-     res.header('Access-Control-Allow-Credentials', true);
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Origin,X-Requested-With,Content-Type,Accept,content-type');
-    next();
-});
 
-//app.use(cors());
+// app.use((req, res, next) => {
+//     res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+//      res.header('Access-Control-Allow-Credentials', true);
+//     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+//     res.header('Access-Control-Allow-Headers', 'Origin,X-Requested-With,Content-Type,Accept,content-type');
+//     next();
+// });
+
+app.use(cors());
 app.use(express.json());
 
 
