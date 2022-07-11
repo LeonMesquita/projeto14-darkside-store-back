8f5c70ba066a0c8096fd805ef243de35264041ad
LeonMesquita <leonmesquita16@gmail.com>
2022-07-11 11:34:34 -0300

    realizar pedidos

--- src/routes/productRouter.js
+++ src/routes/productRouter.js
@@ -1,5 +1,5 @@
 import { Router} from 'express';
-import {createProduct, getProducts, addItems, getCart, favoriteItem, getFavorites} from '../controllers/productController.js';
+import {createProduct, getProducts, addItems, getCart, favoriteItem, getFavorites, payOrder, deleteCart, getOrders} from '../controllers/productController.js';
 
 const router = Router();
 router.post('/products', createProduct);
@@ -8,4 +8,6 @@ router.post('/cart', addItems);
 router.get('/cart', getCart);
 router.post('/favorite/:action', favoriteItem);
 router.get('/favorite', getFavorites);
+router.post('/order', payOrder);
+router.get('/order', getOrders);
 export default router;
